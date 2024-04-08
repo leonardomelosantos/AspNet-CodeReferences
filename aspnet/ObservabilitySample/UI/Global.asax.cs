19bcb328136e7ee4bc61982b2dcefd6d534a24d6
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2024-04-07 23:02:32 -0300

    Basic OpenTelemetry  configuration on Global.asax

--- aspnet/ObservabilitySample/UI/Global.asax.cs
+++ aspnet/ObservabilitySample/UI/Global.asax.cs
@@ -1,3 +1,5 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,14 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using OpenTelemetry.Trace;
 
 namespace ObservabilitySample.WebApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private TracerProvider _tracerProvider;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +23,22 @@ namespace ObservabilitySample.WebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+
+            // Other configuration, like adding an exporter and setting resources
+            .AddConsoleExporter()
+            .AddSource("my-service-name")
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault()
+                    .AddService(serviceName: "my-service-name", serviceVersion: "1.0.0"))
+            .Build();
+        }
+
+        protected void Application_End()
+        {
+            _tracerProvider?.Dispose();
         }
     }
 }
